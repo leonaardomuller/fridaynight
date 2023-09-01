463e5bb8acf4fe173348bbf7d0834041d8cff72b
leonaardomuller <leonaardo.muller@gmail.com>
2023-08-31 22:16:54 -0300

    feat: building carousel

--- src/screens/Map/index.tsx
+++ src/screens/Map/index.tsx
@@ -1,13 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, View, Platform, PermissionsAndroid } from "react-native";
-import { HStack } from "native-base";
+import React, { useEffect, useState, useRef } from "react";
+import {
+  StyleSheet,
+  Platform,
+  PermissionsAndroid,
+  useWindowDimensions,
+} from "react-native";
+import { HStack, View } from "native-base";
 import MapView, { Marker, Circle } from "react-native-maps";
 import { CustomMarker } from "./components/CustomMarker";
 import Geolocation from "react-native-geolocation-service";
 import { ButtonGoBack } from "../../components/ButtonGoBack";
 import { SearchInput } from "./components/SearchInput";
 import { Card } from "./components/Carousel/components/Card";
+import Carousel from "react-native-snap-carousel";
+
 export function Map() {
+  const carouselRef = useRef(null);
+  const { width } = useWindowDimensions();
+
   const [initialRegion, setInitialRegion] = useState(null);
   const [region, setRegion] = useState({
     latitude: 0,
@@ -16,24 +26,59 @@ export function Map() {
     longitudeDelta: 0.005,
   });
   const [location, setLocation] = useState(null);
-
   const [markers, setMarkers] = useState([]);
 
   useEffect(() => {
     requestLocationPermission();
   }, []);
-  //REACT NATIVE MAPS
-  //https://blog.rocketseat.com.br/construindo-um-app-com-mapas-usando-react-native-maps-e-mapbox/
-  //REACT NATIVE SNAP CAROUSEL
+
+  const setLocationData = (latitude, longitude) => {
+    const dif = 0.003;
+    setMarkers([
+      createMarker("Event 1", latitude + dif, longitude + dif),
+      createMarker("Event 2", latitude - dif, longitude - dif),
+      createMarker("Event 3", latitude + dif, longitude + dif * 7),
+      createMarker("Event 4", latitude - dif, longitude - dif * 7),
+      createMarker("Event 5", latitude + dif, longitude + dif * 10),
+      createMarker("Event 6", latitude - dif, longitude - dif * 10),
+    ]);
+    setInitialRegion(createRegion(latitude, longitude));
+    setRegion({ ...region, latitude, longitude });
+    setLocation({ latitude, longitude });
+  };
+
+  const createMarker = (name, latitude, longitude) => ({
+    name,
+    location: {
+      latitude,
+      longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+  });
+
+  const createRegion = (latitude, longitude) => ({
+    latitude,
+    longitude,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const getCurrentLocation = () => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLocationData(latitude, longitude);
+      },
+      (error) => console.log(error),
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
+  };
 
   const requestLocationPermission = async () => {
-    if (Platform.OS === "android") {
-      await requestAndroidLocationPermission();
-    } else if (Platform.OS === "ios") {
-      await requestIOSLocationPermission();
-    } else {
-      getCurrentLocation();
-    }
+    Platform.OS === "android"
+      ? await requestAndroidLocationPermission()
+      : await requestIOSLocationPermission();
   };
 
   const requestAndroidLocationPermission = async () => {
@@ -68,55 +113,6 @@ export function Map() {
     }
   };
 
-  const getCurrentLocation = () => {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        const dif = 0.003;
-        const { latitude, longitude } = position.coords;
-        console.log(latitude, longitude);
-        setMarkers([
-          {
-            name: "Event 1",
-            location: {
-              latitude: latitude + dif,
-              longitude: longitude + dif,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            },
-          },
-          {
-            name: "Event 2",
-            location: {
-              latitude: latitude - dif,
-              longitude: longitude - dif,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            },
-          },
-        ]);
-        setInitialRegion({
-          latitude,
-          longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        });
-
-        setRegion({
-          ...region,
-          latitude,
-          longitude,
-        });
-
-        setLocation({
-          latitude,
-          longitude,
-        });
-      },
-      (error) => console.log(error),
-      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
-  };
-
   return (
     <View style={styles.container}>
       <HStack
@@ -133,15 +129,13 @@ export function Map() {
       <MapView
         style={styles.map}
         initialRegion={initialRegion}
-        onRegionChangeComplete={(region) => setRegion(region)}
+        onRegionChangeComplete={setRegion}
       >
-        {markers.map((data) => {
-          return (
-            <Marker key={data.name} coordinate={data.location}>
-              <CustomMarker name={data.name} />
-            </Marker>
-          );
-        })}
+        {markers.map((data) => (
+          <Marker key={data.name} coordinate={data.location}>
+            <CustomMarker name={data.name} />
+          </Marker>
+        ))}
         <Circle
           center={location}
           radius={50}
@@ -149,7 +143,15 @@ export function Map() {
           strokeColor="rgba(255, 158, 0, 0.5)"
         />
       </MapView>
-      <Card />
+      <View justifyContent={"center"} alignItems={"center"} top="55%">
+        <Carousel
+          ref={carouselRef}
+          data={markers}
+          renderItem={({ item }) => <Card />}
+          sliderWidth={width}
+          itemWidth={width}
+        />
+      </View>
     </View>
   );
 }
@@ -160,7 +162,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
   },
-  safeArea: { flex: 1 },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
